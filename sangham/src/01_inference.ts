2be9d211804ad505aae2eb5b8690e0dbb6e303b0
MuizzRafid <muizzrafid1@gmail.com>
2026-07-25 23:11:34 +0600

    new typescript tutorial added

---
+++ sangham/src/01_inference.ts
@@ -0,0 +1,13 @@
+//goal -> understand when to let typescript infer types
+//ts -> js very well
+//when ts will writes the types / u r going to write
+
+let count = 0; //ts sees number
+const site = "acedevhub"; //it seestthe exact literal "acedevhub"
+
+//over annotation isn't bad -> just too much noisy
+//because ts know what it is
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
---
+++ sangham/src/02_primitives.ts
@@ -0,0 +1,12 @@
+let username: string = "muizz";
+
+let age: number = 40;
+
+let isCreator: boolean = true;
+
+const big: bigint = 2n ** 63n - 1n;
+
+//operator '+' cannot applied to types 'bigint' and 'number'
+// const mixed=big+age;
+
+const TOKEN: unique symbol = Symbol("TOKEN");
