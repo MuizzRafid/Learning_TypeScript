b003c0240e3bf7e2a4ffe7001ef84739b4c35e28
MuizzRafid <muizzrafid1@gmail.com>
2026-08-10 10:44:26 +0600

    new description added

--- sangham/src/04_objects.ts
+++ sangham/src/04_objects.ts
@@ -8,7 +8,13 @@ const user1: User = { id: "1", name: "muizz", createdAt: new Date() };
 //not getting error because email is optional
 
 type Count = { [k: string]: number };
+//creating a type that describes what all key value pairs in an object must look like
 type Count1 = Record<"likes" | "views" | "shares", number>;
+
+//Record is build in generic type
+//Count1 means Record< what are the keys?, what type should the values be?>
+//the union inside the record not work like using in variable they Means
+//"Here is the complete set of keys that this object must have"
 
 const c1: Count = { july: 1 };
 const c2: Count1 = { likes: 5, views: 3, shares: 7 };
